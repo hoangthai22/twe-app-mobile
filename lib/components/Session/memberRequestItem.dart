4c99775042f5dc8e51d00f22274e82f75ee9eb79
hoangthai22 <hoangthai22tv@gmail.com>
2022-03-05 23:21:38 +0700

    update

--- lib/components/Session/memberRequestItem.dart
+++ lib/components/Session/memberRequestItem.dart
@@ -13,63 +13,68 @@ class MemberRequestItem extends StatelessWidget {
     // TODO: implement build
     return Stack(
       children: [
-        Container(
-          height: 115,
-          decoration: isBorderBottom
-              ? const BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(color: Colors.black12, width: 1.0)),
-                )
-              : null,
-          padding: EdgeInsets.only(bottom: 15, top: 15),
-          child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Container(
-              margin: EdgeInsets.only(right: 15),
-              child: CircleAvatar(
-                radius: 35, // Image radius
-                backgroundImage: NetworkImage(item.image!),
+        InkWell(
+          onTap: () {
+            Navigator.pushNamed(context, '/member-detail');
+          },
+          child: Container(
+            height: 115,
+            decoration: isBorderBottom
+                ? const BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.black12, width: 1.0)),
+                  )
+                : null,
+            padding: EdgeInsets.only(bottom: 15, top: 15),
+            child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Container(
+                margin: EdgeInsets.only(right: 15),
+                child: CircleAvatar(
+                  radius: 35, // Image radius
+                  backgroundImage: NetworkImage(item.image!),
+                ),
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Title(
-                    color: Colors.black,
-                    child: Text(
-                      item.fullname!,
-                      style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600),
-                    )),
-                Container(
-                    margin: EdgeInsets.only(top: 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(right: 5),
-                          child: Icon(
-                            Icons.school,
-                            color: MaterialColors.primary,
-                            size: 18,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Title(
+                      color: Colors.black,
+                      child: Text(
+                        item.fullname!,
+                        style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      )),
+                  Container(
+                      margin: EdgeInsets.only(top: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            child: Icon(
+                              Icons.school,
+                              color: MaterialColors.primary,
+                              size: 18,
+                            ),
                           ),
-                        ),
-                        Text(
-                          item.listMajor![0].toString(),
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontFamily: 'Roboto',
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400),
-                        ),
-                      ],
-                    ))
-              ],
-            ),
-          ]),
+                          Text(
+                            item.listMajor![0].toString(),
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontFamily: 'Roboto',
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      ))
+                ],
+              ),
+            ]),
+          ),
         ),
         Positioned(
           child: Container(
